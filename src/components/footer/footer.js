6a6b7ff2f6318dddf1da93f71dd64afef4c97f46
jess-m-q-r <jessqr11@gmail.com>
2026-04-30 23:21:49 -0400

    fix: update footer component structure

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,7 +1,12 @@
-export function Footer() {
-  return `
-    <footer class="footer">
-      <p>Los bochitos</p>
-    </footer>
-  `;
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+      <link rel="stylesheet" href="./components/footer/footer.css">
+      <footer class="footer">
+        <p>Los bochitos</p>
+      </footer>
+    `;
+  }
 }
+
+customElements.define('our-footer', Footer);
